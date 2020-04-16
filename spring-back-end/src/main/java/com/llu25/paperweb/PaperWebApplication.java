c8890cc776ebb32c2f233ff179410e5546edae6a
Lintao Lu <llu25@hawk.iit.edu>
2020-04-16 15:47:09 -0500

    generate spring boot

---
+++ spring-back-end/src/main/java/com/llu25/paperweb/PaperWebApplication.java
@@ -0,0 +1,13 @@
+package com.llu25.paperweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperWebApplication.class, args);
+    }
+
+}
---
+++ spring-back-end/src/test/java/com/llu25/paperweb/PaperWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llu25.paperweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
