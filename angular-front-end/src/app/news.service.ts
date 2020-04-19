318ab77fe0043e1dd712b5be8db61ae4914031a5
Lintao Lu <llu25@hawk.iit.edu>
2020-04-19 14:14:00 -0500

    fix url bug

--- angular-front-end/src/app/news.service.ts
+++ angular-front-end/src/app/news.service.ts
@@ -9,6 +9,6 @@ export class NewsService {
   constructor(private http: HttpClient) { }
 
   public getNews(type:string, id:number) {
-    return this.http.get('http://161.35.49.253:4200/getNews?id=' + id + '&keyword=' + type);
+    return this.http.get('http://161.35.49.253:8080/getNews?id=' + id + '&keyword=' + type);
   }
 }
