d2b5f3772cef0ff38ffd62143f5458ac9f605e07
Lintao Lu <llu25@hawk.iit.edu>
2020-05-10 22:28:46 -0500

    remove spring securities

--- spring-back-end/src/main/scripts/users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    user_email VARCHAR(100) NOT NULL,
-    user_password VARCHAR(25) NOT NULL,
-    user_name VARCHAR(30) NOT NULL,
-    active BIT(1),
-    roles ENUM('ROLE_USER', 'ROLE_ADMIN') NOT NULL,
-    PRIMARY KEY (user_email)
-);
