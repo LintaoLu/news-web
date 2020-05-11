9825d1921d2549d61b8bf86cb6146c1c4733bb8c
Lintao Lu <llu25@hawk.iit.edu>
2020-05-11 10:04:25 -0500

    ui

--- angular-front-end/src/environments/environment.prod.ts
+++ angular-front-end/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCeql9lk3Q5tiEW7aqOjZ5y28oDoJSnRYI",
+    authDomain: "news-web-46b4a.firebaseapp.com",
+    databaseURL: "https://news-web-46b4a.firebaseio.com",
+    projectId: "news-web-46b4a",
+    storageBucket: "news-web-46b4a.appspot.com",
+    messagingSenderId: "983803333171",
+    appId: "1:983803333171:web:1ea3913d28b836e2d23146",
+    measurementId: "G-1LDBTHYKSL"
+  }
 };
